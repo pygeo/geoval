c283a7f28b698743cddb53008d6f9562af54f60c
Alex Loew <a.loew@gmx.net>
2016-06-15 09:38:53 +0200

    using pyximport now for mintrend compilation

--- geoval/statistic/mintrend.pyx
+++ geoval/statistic/mintrend.pyx
@@ -7,10 +7,6 @@ cimport numpy as np
 #~ import numpy as np
 from scipy import stats
 
-import matplotlib
-matplotlib.use('Agg')
-
-import matplotlib.pyplot as plt
 
 ctypedef np.double_t DTYPE_t  # double type for numpy arrays
 
@@ -183,54 +179,5 @@ cdef class TrendModel(object):
         assert False
         p = 1.
         return p
-
-
-#~ plt.close('all')
-
-
-
-
-t = np.arange(1951,2001,1).astype('float')
-N = 100
-trends = np.arange(1.,21.,0.1)  # todo: do faster by Newton itteration --> not all trends need to be calculated
-
-means = np.linspace(100.,1000.,20)
-cvs = np.linspace(0.1,1.,10)
-
-nmeans = len(means)
-ncvs = len(cvs)
-
-MT = np.ones((nmeans,ncvs))*np.nan
-
-for i in xrange(nmeans):
-
-    for j in xrange(ncvs):
-        M = Mintrend(t, means[i], cvs[j], trends, N)
-        #M = Mintrend(t, 600., 0.2, trends=trends, N=N)
-        MT[i,j] = M.get_mintrend()
-        print means[i], cvs[j] #, MT[i,j]
-
-
-# generate plot
-levels=np.arange(2.,20,2)
-f = plt.figure()
-ax = f.add_subplot(111)
-CS = ax.contour(means, cvs, MT.T, levels=levels)
-plt.clabel(CS, inline=1, fontsize=10)
-ax.grid()
-ax.set_xlabel('mean value')
-ax.set_ylabel('CV')
-ax.set_title('minimum detectable trend')
-f.savefig('mintrend_contour.png', dpi=300)
-plt.close('all')
-
-#~ plt.show()
-
-
-#~ T = TrendModel(t, 600., 0.2, 10./10., N)  # todo 10mm/decade ...
-#~ P = T.calc_trend_significances()
-#~
-#~ pthres = 0.05
-#~ print 'Fraction: ', len(P[P<pthres]) / float(N)
 
 
