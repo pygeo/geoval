108a261646c71616d1e6dc686921b00629aeee0d
Alex <alexander.loew@lmu.de>
2017-03-13 17:37:19 +0100

    adapted license statement in setup.py

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ setup(name='geoval',
           'Topic :: Scientific/Engineering :: Visualization',
 
           # Pick your license as you wish (should match "license" above)
-          'License :: OSI Approved :: Apache 2.0',
+          # :w'License :: OSI Approved :: Apache 2.0',
 
           # Specify the Python versions you support here. In particular, ensure
           # that you indicate whether you support Python 2, Python 3 or both.
